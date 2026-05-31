54db2862f6493b4c162c3a686d96b12cef5baeea
DILSHODBEK <dilshodsattar26@gmail.com>
2026-06-01 00:04:51 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class routerSecryptWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = routerSecryptWorker()
+    print("Worker engine initialized.")
